2b2cf17ad897ef3c42d76a14608d88d5ed01caea
Andrew Colannino <akcolannino@email.neit.edu>
2020-07-03 18:07:18 -0400

    scss modification, emoji in the tsx!!!!!

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,6 @@ import { h, render } from 'preact';
 import './style.scss'
 
 const App = () =>
-	<h2>Hello friends! This code is type-safe!!!</h2>;
+	<h2>Hello friends! This code is type-safe!! 🥳</h2>;
 
 render(<App />, document.getElementById('root'));
