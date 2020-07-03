a88c572b35e98190512f01efb6a2c38899787d44
Andrew Colannino <akcolannino@email.neit.edu>
2020-07-03 18:35:30 -0400

    Simple increment / decrement buttons

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,21 @@
 import { h, render } from 'preact';
+import { useState } from 'preact/hooks'
+
 import './style.scss'
 
 const App = () =>
 	<h2>Hello friends! This code is type-safe!! 🥳</h2>;
 
-render(<App />, document.getElementById('root'));
+function Counter() {
+	const [value, setValue] = useState(0);
+
+	return (
+		<div>
+			<div>Counter: {value}</div>
+			<button onClick={() => setValue(value + 1)}>Increment</button>
+			<button onClick={() => setValue(value - 1)}>Decrement</button>
+		</div>
+	)
+}
+
+render(<Counter />, document.getElementById('root'));
