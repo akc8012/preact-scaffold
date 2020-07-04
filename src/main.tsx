4c8117871d2a8cbe9634f79273d18133bf1b2d04
Andrew Colannino <akcolannino@email.neit.edu>
2020-07-03 20:57:55 -0400

    CSS fixies, renaming enum

--- src/main.tsx
+++ src/main.tsx
@@ -5,23 +5,23 @@ import { useState } from 'preact/hooks'
 import '@fortawesome/fontawesome-free/css/all.css';
 import './style.scss'
 
-enum Selection {
+enum Choice {
 	Brush = 'Brush',
 	Eraser = 'Eraser'
 };
 
 function App() {
-	const [value, setValue] = useState(Selection.Brush);
+	const [choice, setChoice] = useState(Choice.Brush);
 
 	return (
 		// TODO: Find out why I can't use <></>
 		<div class='buttons'>
-			<div>Selection: {value}</div>
-			<button class='brush' onClick={() => setValue(Selection.Brush)}>
+			<div>Choice: <b>{choice}</b></div>
+			<button class='brush' onClick={() => setChoice(Choice.Brush)}>
 				<i class='fas fa-paint-brush fa-2x' />
 			</button>
 
-			<button class='eraser' onClick={() => setValue(Selection.Eraser)}>
+			<button class='eraser' onClick={() => setChoice(Choice.Eraser)}>
 				<i class='fas fa-eraser fa-2x' />
 			</button>
 		</div>
