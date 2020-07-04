50f7fd9a44e30fc161bc7f729d88df99383a78c7
Andrew Colannino <akcolannino@email.neit.edu>
2020-07-04 14:05:32 -0400

    button selected class, owo css is stylish

--- src/main.tsx
+++ src/main.tsx
@@ -16,19 +16,29 @@ function App() {
 	return (
 		// TODO: Find out why I can't use <></>
 		<div class='buttons'>
-			<div>Choice: <b>{choice}</b></div>
+			<Button
+				choice={Choice.Brush}
+				selected={choice === Choice.Brush}
+				click={(clicked: Choice) => setChoice(clicked)}
+			/>
 
-			<Button choice={Choice.Brush} click={(clicked: Choice) => setChoice(clicked)} />
-			<Button choice={Choice.Eraser} click={(clicked: Choice) => setChoice(clicked)} />
+			<Button
+				choice={Choice.Eraser}
+				selected={choice === Choice.Eraser}
+				click={(clicked: Choice) => setChoice(clicked)}
+			/>
+
+			<div>Choice: <b>{choice}</b></div>
 		</div>
 	);
 }
 
-function Button(props: { choice: Choice, click: any }) {
+function Button(props: { choice: Choice, selected: boolean, click: any }) {
+	let selected = props.selected ? 'selected' : '';
 	let icon = props.choice === Choice.Brush ? 'paint-brush' : 'eraser';
 
 	return (
-		<button onClick={() => props.click(props.choice)}>
+		<button class={selected} onClick={() => props.click(props.choice)} >
 			<i class={`fas fa-${icon} fa-2x`} />
 		</button>
 	);
